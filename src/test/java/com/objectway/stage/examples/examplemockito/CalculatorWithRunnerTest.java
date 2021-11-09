01c3005fd7bd847f69c7dcd20cdad7a77f7c32aa
Danilo Arcidiacono <danilo.arcidiacono@objectway.it>
2021-11-09 17:49:07 +0100

    Update deprecated class.

--- src/test/java/com/objectway/stage/examples/examplemockito/CalculatorWithRunnerTest.java
+++ src/test/java/com/objectway/stage/examples/examplemockito/CalculatorWithRunnerTest.java
@@ -5,8 +5,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 
